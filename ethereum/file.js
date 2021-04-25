def5db2546b8a7d1a492c0c7eb916b8a470bf4f7
nikgeek <65403527+nikgeek@users.noreply.github.com>
2021-04-25 21:23:00 +0530

    Update file.js

--- ethereum/file.js
+++ ethereum/file.js
@@ -3,7 +3,7 @@ import compiledFactory from './build/CampaignFactory.json';
 //create a instance of a deployed contract by providing contracts ABI and deployed contracts address
 const instance = new web3.eth.Contract(
   JSON.parse(compiledFactory.interface),
-  '0x3cAC8B9bF7c3FEFD7fbC02645b79B3B6bbB7C98A'
+  '0xA87F0cF9659Fad89b06Dd6EA4E72E3699F485569'
 );
 
 export default instance;
